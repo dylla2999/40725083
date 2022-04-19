3880b1c0060f3c2ee5ed58374bc75718e99192bb
Dylla <95353989+dylla2999@users.noreply.github.com>
2022-04-19 15:52:48 +0800

    程式碼一直出問題
    gg

---
+++ WebGame/Assets/SCripts/TopDownController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    [SerializeField, Header("移動速度"), Range(0, 500)]
+    private float speed = 1.5f;
+    private string parameterRun = "TriggerRun";
+    private string parameterDead = "Death";
+    private Animator ani;
+    private Rigidbody2D rig;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigibody2D>();
+
+    }
+}
